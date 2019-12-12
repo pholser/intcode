c24cc5bd786efee24faf26b2b4347237281b9ad6
Paul Holser <holser@railroad19.com>
2019-12-11 22:41:02 -0600

    d9p2

--- src/test/java/com/pholser/intcode/Day9Part1And2Test.java
+++ src/test/java/com/pholser/intcode/Day9Part1And2Test.java
@@ -10,9 +10,8 @@ import java.util.concurrent.LinkedBlockingQueue;
 import static java.util.Collections.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class Day9Part1Test {
-    @Test
-    void part1() throws Exception {
+class Day9Part1And2Test {
+    @Test void part1() throws Exception {
         BlockingQueue<String> ioBuffer = new LinkedBlockingQueue<>();
         IntcodeComputer computer = new IntcodeComputer(30000, ioBuffer);
 
@@ -22,6 +21,19 @@ class Day9Part1Test {
 
         computer.run();
 
-        assertEquals(singletonList("..."), computer.outBuffer());
+        assertEquals(singletonList("2890527621"), computer.outBuffer());
+    }
+
+    @Test void part2() throws Exception {
+        BlockingQueue<String> ioBuffer = new LinkedBlockingQueue<>();
+        IntcodeComputer computer = new IntcodeComputer(30000, ioBuffer);
+
+        ioBuffer.put("2");
+        computer.loadProgram(
+            new FileInputStream("src/test/resources/day9-input.intcode"));
+
+        computer.run();
+
+        assertEquals(singletonList("66772"), computer.outBuffer());
     }
 }
