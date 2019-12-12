f3c6794a6971ec52427589fcb99f5c46d43e4d3a
Paul Holser <holser@railroad19.com>
2019-12-11 21:54:39 -0600

    d7p2

--- src/test/java/com/pholser/intcode/Day7Part1And2Test.java
+++ src/test/java/com/pholser/intcode/Day7Part1And2Test.java
@@ -23,6 +23,12 @@ class Day7Part1And2Test {
             runWithAllPhaseSettings(0, 4, this::runPart1WithPhaseSettings));
     }
 
+    @Test void part2() {
+        assertEquals(
+            34579864,
+            runWithAllPhaseSettings(5, 9, this::runPart2WithPhaseSettings));
+    }
+
     private int runWithAllPhaseSettings(
         int lo,
         int hi,
@@ -83,7 +89,58 @@ class Day7Part1And2Test {
         }
     }
 
-    Collection<List<String>> phaseSettingPermutations(int lo, int hi) {
+    private int runPart2WithPhaseSettings(List<String> settings) {
+        try {
+            BlockingQueue<String> in1 = new LinkedBlockingQueue<>();
+            in1.put(settings.get(0));
+            BlockingQueue<String> out1in2 = new LinkedBlockingQueue<>();
+            out1in2.put(settings.get(1));
+            BlockingQueue<String> out2in3 = new LinkedBlockingQueue<>();
+            out2in3.put(settings.get(2));
+            BlockingQueue<String> out3in4 = new LinkedBlockingQueue<>();
+            out3in4.put(settings.get(3));
+            BlockingQueue<String> out4in5 = new LinkedBlockingQueue<>();
+            out4in5.put(settings.get(4));
+
+            IntcodeComputer computer1 =
+                new IntcodeComputer(10000, in1, out1in2);
+            computer1.loadProgram(
+                new FileInputStream("src/test/resources/day7-input.intcode"));
+            IntcodeComputer computer2 =
+                new IntcodeComputer(10000, out1in2, out2in3);
+            computer2.loadProgram(
+                new FileInputStream("src/test/resources/day7-input.intcode"));
+            IntcodeComputer computer3 =
+                new IntcodeComputer(10000, out2in3, out3in4);
+            computer3.loadProgram(
+                new FileInputStream("src/test/resources/day7-input.intcode"));
+            IntcodeComputer computer4 =
+                new IntcodeComputer(10000, out3in4, out4in5);
+            computer4.loadProgram(
+                new FileInputStream("src/test/resources/day7-input.intcode"));
+            IntcodeComputer computer5 =
+                new IntcodeComputer(10000, out4in5, in1);
+            computer5.loadProgram(
+                new FileInputStream("src/test/resources/day7-input.intcode"));
+
+            in1.put("0");
+
+            new Thread(computer1).start();
+            new Thread(computer2).start();
+            new Thread(computer3).start();
+            new Thread(computer4).start();
+            computer5.run();
+
+            return Integer.parseInt(computer5.outBuffer().element());
+        } catch (InterruptedException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Collection<List<String>> phaseSettingPermutations(
+        int lo,
+        int hi) {
+
         List<String> settings =
             IntStream.rangeClosed(lo, hi)
                 .mapToObj(Integer::toString)
